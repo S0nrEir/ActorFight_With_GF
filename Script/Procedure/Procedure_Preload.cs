083a7ddf0c93a8508d052b9906939b924f224afe
YangHaoChen <x73901@live.com>
2022-07-11 17:01:16 +0800

    添加启动流程和预加载流程，设置启动流程为launchProcedure

---
+++ Script/Procedure/Procedure_Preload.cs
@@ -0,0 +1,30 @@
+﻿using GameFramework.Fsm;
+using GameFramework.Procedure;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Aquila.Procedure
+{
+    /// <summary>
+    /// 预加载流程
+    /// </summary>
+    public class Procedure_Prelaod : ProcedureBase
+    {
+        protected override void OnInit( IFsm<IProcedureManager> procedureOwner )
+        {
+            base.OnInit( procedureOwner );
+        }
+
+        protected override void OnEnter( IFsm<IProcedureManager> procedureOwner )
+        {
+            base.OnEnter( procedureOwner );
+        }
+
+        protected override void OnLeave( IFsm<IProcedureManager> procedureOwner, bool isShutdown )
+        {
+            base.OnLeave( procedureOwner, isShutdown );
+        }
+    }
+
+}
