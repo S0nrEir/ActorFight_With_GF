fd10674e5434a5eaae1ee05cdf9e8fe567ea94ab
YangHaoChen <x73901@live.com>
2022-07-12 16:44:05 +0800

    修改配表，测试读表流程

--- Script/Procedure/Procedure_Preload.cs
+++ Script/Procedure/Procedure_Preload.cs
@@ -1,8 +1,11 @@
-﻿using GameFramework.Fsm;
+﻿using Bright.Serialization;
+using GameFramework.Fsm;
 using GameFramework.Procedure;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityGameFramework.Runtime;
 
 namespace Aquila.Procedure
 {
@@ -19,11 +22,30 @@ namespace Aquila.Procedure
         protected override void OnEnter( IFsm<IProcedureManager> procedureOwner )
         {
             base.OnEnter( procedureOwner );
+            //加载数据表
+            LoadDataTables();
         }
 
         protected override void OnLeave( IFsm<IProcedureManager> procedureOwner, bool isShutdown )
         {
             base.OnLeave( procedureOwner, isShutdown );
+        }
+
+        /// <summary>
+        /// 加载数据表
+        /// </summary>
+        private void LoadDataTables()
+        {
+            var table_ctor = typeof( cfg.Tables ).GetConstructors()[0];
+            var loader = new System.Func<string, ByteBuf>( (file)=>
+            {
+                return new ByteBuf( File.ReadAllBytes( $"{Application.dataPath}/Res/DataTables/{file}.bytes" ) );
+            } );
+            var tables = ( cfg.Tables ) table_ctor.Invoke( new object[] { loader } );
+            foreach ( var meta in tables.TbItem.DataList )
+            {
+                Log.Info( $"meta info,desc:{meta.Desc},id:{meta.Id}" ,LogColorTypeEnum.White);
+            }
         }
     }
 
