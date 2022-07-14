b305d4139d46d9d984e761da6f86f6068a3f2ab7
YangHaoChen <x73901@live.com>
2022-07-14 18:13:22 +0800

    添加empty地块；修改测试场景；添加object类型

---
+++ Script/Object/Object_Terrain.cs
@@ -0,0 +1,41 @@
+﻿using GameFramework.ObjectPool;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Aquila.ObjectPool
+{
+    /// <summary>
+    /// 地块对象
+    /// </summary>
+    public class Object_Terrain : Aquila_Object_Base
+    { 
+        /// <summary>
+        /// 获取对象
+        /// </summary>
+        protected override void OnSpawn()
+        {
+            base.OnSpawn();
+        }
+
+        /// <summary>
+        /// 回收
+        /// </summary>
+        protected override void OnUnspawn()
+        {
+            base.OnUnspawn();
+        }
+
+        /// <summary>
+        /// 释放
+        /// </summary>
+        protected override void Release( bool isShutdown )
+        {
+            base.Release( isShutdown );
+        }
+
+
+
+    }
+
+}
