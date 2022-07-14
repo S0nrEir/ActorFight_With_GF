aeb9c13af85611f4769e27c1042cc54b6c53784b
YangHaoChen <x73901@live.com>
2022-07-14 16:35:20 +0800

    提交开始流程，更新测试场景

---
+++ Script/Procedure/Procedure_Start.cs
@@ -0,0 +1,24 @@
+﻿using GameFramework.Fsm;
+using GameFramework.Procedure;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Aquila.Procedure
+{
+    /// <summary>
+    /// 开始流程，加载游戏后的主界面
+    /// </summary>
+    public class Procedure_Start : ProcedureBase
+    {
+        protected override void OnEnter( IFsm<IProcedureManager> procedureOwner )
+        {
+            base.OnEnter( procedureOwner );
+        }
+
+        protected override void OnLeave( IFsm<IProcedureManager> procedureOwner, bool isShutdown )
+        {
+            base.OnLeave( procedureOwner, isShutdown );
+        }
+    }
+}
