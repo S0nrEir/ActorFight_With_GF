e4d5b7d009f0d2ce4a89b4360ca3efd862515365
YangHaoChen <x73901@live.com>
2022-07-15 18:07:21 +0800

    GlobalVar、GameConfig剔除无用代码，增加相应的层级和全局变量

--- Script/Config/GlobalVar.cs
+++ Script/Config/GlobalVar.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameFramework;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,6 +20,32 @@ namespace Aquila.Config
         /// 无效ID
         /// </summary>
         public const int INVALID_ID = -1;
+
+        /// <summary>
+        /// 场景主相机
+        /// </summary>
+        public static Camera Main_Camera
+        {
+            get
+            {
+                if ( _main_camera == null )
+                {
+                    var cameraGO = GameObject.FindGameObjectWithTag( "MainCamera" );
+                    if ( cameraGO == null )
+                        throw new GameFrameworkException( "faild to find MainCamera GameObject!" );
+
+                    _main_camera = cameraGO.GetComponent<Camera>();
+                    if(_main_camera == null)
+                        throw new GameFrameworkException( "faild to get MainCamera!" );
+                }
+                return _main_camera;
+            }
+        }
+
+        /// <summary>
+        /// 主相机
+        /// </summary>
+        private static Camera _main_camera = null;
     }
 
 }
