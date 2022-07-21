57fe075315323e71d9afd423952ada688f13a199
YangHaoChen <x73901@live.com>
2022-07-21 18:01:24 +0800

    增加lua脚本工具类

---
+++ Script/Common/Tools/Tools.Lua.cs
@@ -0,0 +1,30 @@
+﻿using UnityGameFramework.Runtime;
+
+namespace Aquila
+{
+    public static partial class Tools
+    {
+        /// <summary>
+        /// Lua工具类
+        /// </summary>
+        public static class Lua
+        {
+            /// <summary>
+            /// 根据asset路径返回对应的脚本名称，失败返回string.empty
+            /// </summary>
+            public static string GetScriptName( string asset_path )
+            {
+                if ( string.IsNullOrEmpty( asset_path ) )
+                {
+                    Log.Warning( "asset path is null" );
+                    return string.Empty;
+                }
+                var temp = asset_path.Split( '/' );
+                if ( temp is null || temp.Length == 0 )
+                    return string.Empty;
+
+                return temp[temp.Length - 1];
+            }
+        }
+    }
+}
