f3943ce00586f2f15635979e0079a8963fe15838
YangHaoChen <x73901@live.com>
2023-01-03 18:34:30 +0800

    asset_packer

--- Script/Editor/Aquila/Asset_Setting_Packer.cs
+++ Script/Editor/Aquila/Asset_Setting_Packer.cs
@@ -1,4 +1,3 @@
-using GameFramework;
 using ICSharpCode.SharpZipLib.Zip;
 using System;
 using System.IO;
@@ -15,7 +14,7 @@ namespace Aquila.Editor
         /// <summary>
         /// 打包文件夹
         /// </summary>
-        private static string[] _dicts = new string[]
+        private static string[] _include_dic = new string[]
         {
             "DataTable",
             "Note",
@@ -25,6 +24,21 @@ namespace Aquila.Editor
         };
 
         /// <summary>
+        /// 创建的zip文件名
+        /// </summary>
+        private static string _file_name = @"/AssetSetting_Test.zip";
+
+        /// <summary>
+        /// 完整路径
+        /// </summary>
+        private static string _create_path = Application.dataPath + _file_name;
+
+        /// <summary>
+        /// 默认的压缩级别
+        /// </summary>
+        private const int _default_compress_level = 5;
+
+        /// <summary>
         /// 打包AssetSetting为Zip，并且放到application目录下
         /// </summary>
         [MenuItem( "Aquila/PackAssetSetting" )]
@@ -32,13 +46,23 @@ namespace Aquila.Editor
         {
             PrevOp();
 
-            var create_path = Application.dataPath + "AssetSetting.zip";
-            using ( ZipOutputStream stream = new ZipOutputStream( File.Create( create_path ) ) )
+            using ( ZipOutputStream stream = new ZipOutputStream( File.Create( _create_path ) ) )
             {
-                foreach ( var dic in _dicts )
+                var size = 0l;
+                stream.SetLevel( _default_compress_level );
+                foreach ( var dic in _include_dic )
                 {
-                    ZipDict( dic, stream );
+                    size += ZipDict( dic, stream );
                 }
+                stream.Flush();
+                //try
+                //{
+                //    stream.Flush();
+                //}
+                //catch ( Exception err )
+                //{
+                //    Debug.LogError( err.Message );
+                //}
             }
         }
 
@@ -56,60 +80,75 @@ namespace Aquila.Editor
         }
 
         /// <summary>
-        /// 压缩文件夹
-        /// </summary>
-        //private static void ZipDicts(string[] dicts)
-        //{
-        //    var create_path = Application.dataPath + "AssetSetting.zip";
-        //    using ( ZipOutputStream stream = new ZipOutputStream( File.Create( create_path ) ) )
-        //    {
-        //        //压缩级别
-        //        stream.SetLevel( 0 );
-        //        byte[] buffer = new byte[4096];
-        //        string[] files_in_dict = null;
-        //        string dict_path;
-        //        foreach ( var dict in dicts )
-        //        {
-        //            dict_path = @System.Environment.CurrentDirectory + "/" + dict;
-        //            if ( !Directory.Exists( dict_path ) )
-        //            {
-        //                Debug.Log( $"<color=white>doesnt exsits dict {dict},skip</color>" );
-        //                continue;
-        //            }
-
-        //            files_in_dict = Directory.GetFileSystemEntries( dict_path );
-        //            ZipDicts( files_in_dict, stream );
-        //        }
-        //    }
-        //}
-
-        /// <summary>
         /// 压缩目录
         /// </summary>
-        private static void ZipDict( string dict, ZipOutputStream stream )
+        private static long ZipDict( string dict, ZipOutputStream stream )
         {
-            var dicts = Directory.GetDirectories( @System.Environment.CurrentDirectory + "/" + dict );
-            if ( dicts.Length != 0 )
+            var size = 0l;
+            //遍历每一项，如果是目录就继续，如果不是，直接压缩
+            var files_in_dict = Directory.GetFiles( dict );
+            foreach ( var file in files_in_dict )
+                size += ZipFile( stream, file );
+
+            var dicts_in_dict = Directory.GetDirectories( dict );
+            foreach ( var temp_dict in dicts_in_dict )
+                ZipDict( temp_dict, stream );
+
+            return size;
+            //var dicts = Directory.GetDirectories( @System.Environment.CurrentDirectory + "\\" + dict );
+
+            //if ( dicts.Length != 0 )
+            //{
+            //    foreach ( var t in dicts )
+            //        ZipDict( t, stream );
+            //}
+
+            //var files = Directory.GetFiles( @System.Environment.CurrentDirectory + "/" + dict );
+            //string file_name;
+            //int size = 0;
+            //foreach ( var file in files )
+            //{
+            //    file_name = @System.Environment.CurrentDirectory + "/" + dict + "/" + file;
+            //}
+        }
+
+        /// <summary>
+        /// 压缩单个文件
+        /// </summary>
+        private static long ZipFile( ZipOutputStream stream, string file )
+        {
+            if ( !File.Exists( file ) )
             {
-                foreach ( var t in dicts )
-                    ZipDict( t, stream );
+                Debug.Log( "<color=red>file doesnt exists--->{file}</color>" );
+                return 0;
+            }
+            //512000000 bytes
+            FileInfo file_info = new FileInfo( file );
+            byte[] buffer = new byte[file.Length];//文件如果太大，大于缓冲区大小，会报错，如果又这种情况则需要循环读取
+            var size = 0l;
+            //size = fs.Read( buffer, 0, buffer.Length );
+            size = file_info.Length;
+            if ( size == 3488 || size == 21 )
+                ;
+            ZipEntry entry = new ZipEntry( file );
+            entry.DateTime = DateTime.Now;
+            entry.Size = size;
+            stream.PutNextEntry( entry );
+            using ( FileStream fs = File.OpenRead( file ) )
+            {
+                ICSharpCode.SharpZipLib.Core.StreamUtils.Copy( fs, stream, new byte[4096] );
+                //stream.Write( buffer, 0, buffer.Length );
+                //stream.Write( buffer, 0, size );
+            }
+            stream.CloseEntry();
+
+            if ( size == 0 )
+            {
+                Debug.Log( "<color=red>file size is 0</color>" );
+                return 0;
             }
 
-            var files = Directory.GetFiles( @System.Environment.CurrentDirectory + "/" + dict );
-            string file_name;
-            foreach ( var file in files )
-            {
-                file_name = @System.Environment.CurrentDirectory + "/" + dict + "/" + file;
-                byte[] buffer = new byte[4096];
-                ZipEntry entry = new ZipEntry( file );
-                entry.DateTime = DateTime.Now;
-                stream.PutNextEntry( entry );
-                //写入文件到zip内
-                using ( FileStream fs = File.OpenRead( file_name ) )
-                {
-                    
-                }
-            }
+            return size;
         }
     }
 }
