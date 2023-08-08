acd33598cb9d880274ef47f25c46be22b6f7163f
YangHaoChen <x73901@live.com>
2023-08-08 16:53:42 +0800

    修改actor周期函数中不合理的字段初始化和reset

--- Script/Modules/Proxy/Module_ProxyActor.Mgr.cs
+++ Script/Modules/Proxy/Module_ProxyActor.Mgr.cs
@@ -39,66 +39,88 @@ namespace Aquila.Module
 
             var res = TryGet( actor.ActorID );
             if ( !res.has )
-                return Register( actor, new Addon_Base[] { addon } );
-            else
-            {
-                var ins = res.instance;
-                ins.AddAddon( addon );
-                //AddToAddonSystem( addon );
-                return (true, ins);
-            }
-        }
-
-        public (bool succ, ActorInstance instance) Register( Actor_Base actor )
-        {
-            if ( actor is null )
-            {
-                Log.Warning( "<color=yellow>actor is null.</color>" );
                 return (false, null);
-            }
 
-            if ( Contains( actor.ActorID ) )
-            {
-                Log.Warning( $"<color=yellow>proxy has contains actor,id={actor.ActorID}.</color>" );
-                return (false, null); ;
-            }
-
-            var actorCase = ReferencePool.Acquire<ActorInstance>();
-            actorCase.Setup( actor );
-            _proxyActorDic.Add( actor.ActorID, actorCase );
-            //todo:整理两个register接口为一个
-            _registered_id_set.Add( actor.ActorID );
-            return (true, actorCase);
+            var ins = res.instance;
+            ins.AddAddon( addon );
+            return (true, ins);
         }
 
         /// <summary>
-        /// 将actor注册到代理中，成功返回true
+        /// 将一个actor添加到instance管理模块中
         /// </summary>
-        public (bool succ, ActorInstance instance) Register( Actor_Base actor, Addon_Base[] addons )
+        public (bool regSucc, ActorInstance instance) Register( Actor_Base actor )
         {
             if ( actor is null )
             {
-                Log.Warning( "<color=yellow>actor is null.</color>" );
+                Log.Warning( "<color=yellow>Module_ProxyActor.Register()--->actor is null.</color>" );
                 return (false, null);
             }
 
             if ( Contains( actor.ActorID ) )
             {
-                Log.Warning( $"<color=yellow>proxy has contains actor,id={actor.ActorID}.</color>" );
+                Log.Warning( $"<color=yellow>Module_ProxyActor.Register()--->proxy has contains actor,id={actor.ActorID}.</color>" );
                 return (false, null); ;
             }
 
             var actorCase = ReferencePool.Acquire<ActorInstance>();
-            //actorCase.Setup( actor, addons );
             actorCase.Setup( actor );
             _proxyActorDic.Add( actor.ActorID, actorCase );
             _registered_id_set.Add( actor.ActorID );
-            //将addon加入组件系统
-            //foreach ( var addon in addons )
-            //    AddToAddonSystem( addon );
 
             return (true, actorCase);
         }
+
+        //public (bool succ, ActorInstance instance) Register( Actor_Base actor )
+        //{
+        //    if ( actor is null )
+        //    {
+        //        Log.Warning( "<color=yellow>actor is null.</color>" );
+        //        return (false, null);
+        //    }
+
+        //    if ( Contains( actor.ActorID ) )
+        //    {
+        //        Log.Warning( $"<color=yellow>proxy has contains actor,id={actor.ActorID}.</color>" );
+        //        return (false, null); ;
+        //    }
+
+        //    var actorCase = ReferencePool.Acquire<ActorInstance>();
+        //    actorCase.Setup( actor );
+        //    _proxyActorDic.Add( actor.ActorID, actorCase );
+        //    //todo:整理两个register接口为一个
+        //    _registered_id_set.Add( actor.ActorID );
+        //    return (true, actorCase);
+        //}
+
+        ///// <summary>
+        ///// 将actor注册到代理中，成功返回true
+        ///// </summary>
+        //public (bool succ, ActorInstance instance) Register( Actor_Base actor, Addon_Base[] addons )
+        //{
+        //    if ( actor is null )
+        //    {
+        //        Log.Warning( "<color=yellow>actor is null.</color>" );
+        //        return (false, null);
+        //    }
+
+        //    if ( Contains( actor.ActorID ) )
+        //    {
+        //        Log.Warning( $"<color=yellow>proxy has contains actor,id={actor.ActorID}.</color>" );
+        //        return (false, null); ;
+        //    }
+
+        //    var actorCase = ReferencePool.Acquire<ActorInstance>();
+        //    //actorCase.Setup( actor, addons );
+        //    actorCase.Setup( actor );
+        //    _proxyActorDic.Add( actor.ActorID, actorCase );
+        //    _registered_id_set.Add( actor.ActorID );
+        //    //将addon加入组件系统
+        //    //foreach ( var addon in addons )
+        //    //    AddToAddonSystem( addon );
+
+        //    return (true, actorCase);
+        //}
 
         /// <summary>
         /// 注销单个实例
