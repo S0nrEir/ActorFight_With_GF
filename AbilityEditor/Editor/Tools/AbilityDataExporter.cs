f6fb1bc3f0d67ff16c5bae151dc5d3196cdcc838
YangHaoChen <x73901@live.com>
2025-11-26 16:59:31 +0800

    在场景里摆两个木桩

---
+++ AbilityEditor/Editor/Tools/AbilityDataExporter.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Aquila.AbilityEditor
+{
+
+    public static class AbilityDataExporter
+    {
+        /// <summary>
+        /// 导出技能数据
+        /// </summary>
+        public static bool ExportAbilityData()
+        {
+            return true;
+        }
+    }
+}
